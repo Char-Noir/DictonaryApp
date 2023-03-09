5a98d4be9044b50fdaae5d6da937180e8ae67e23
CharNoir <d.buryak2002@gmail.com>
2023-03-09 19:35:38 +0200

    add model for quiz item

---
+++ DictonaryApp/Models/LocalModels/QuizItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictonaryApp.Models.LocalModels
+{
+    public class QuizItem
+    {
+        public int Score = 1;
+        public required string Answer;
+        public required string Question;
+        public bool IsAnsweredCorrectrly = false;
+        public required string AnswerLanguage;
+        public required string QuestionLanguage;
+    }
+}
